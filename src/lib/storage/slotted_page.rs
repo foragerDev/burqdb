ee56d050a54d6eb9463f024d6bc58c285ecc77b8
Mohsan <mohsan0073@gmail.com>
2025-09-03 01:11:57 +0700

    Implement Insert function fro sloted page

--- src/lib/storage/slotted_page.rs
+++ src/lib/storage/slotted_page.rs
@@ -1,14 +1,24 @@
-use std::cell;
+use std::{cell, collections::HashMap, hash::Hash};
 
 use anyhow::Result;
 use bincode::{config::standard, serde::encode_to_vec};
 use serde::{Deserialize, Serialize};
 static PAGE_SIZE: usize = 4096;
 
+#[derive(Serialize, Deserialize)]
+
 pub struct PageHeader<K, V> {
     page_id: u64,
     right: Box<SlottedPage<K, V>>,
     page_size: usize,
+    offset: u16,
+}
+
+#[derive(Serialize, Deserialize)]
+
+pub enum Position {
+    Free(u16),
+    Occupied(u16),
 }
 
 impl<K, V> PageHeader<K, V>
@@ -21,10 +31,12 @@ where
             page_id,
             right,
             page_size: size_of::<PageHeader<K, V>>(),
+            offset: PAGE_SIZE as u16 - 1,
         }
     }
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Cell<K, V> {
     key: K,
     value: V,
@@ -44,7 +56,7 @@ where
         })
     }
 
-    pub fn serialize(&self) -> Result<Vec<u8>> {
+    pub fn _serialize(&self) -> Result<Vec<u8>> {
         let key_bytes = encode_to_vec(&self.key, standard()).unwrap();
         let value_bytes = encode_to_vec(&self.value, standard()).unwrap();
         let mut buffer = Vec::new();
@@ -56,10 +68,12 @@ where
         Ok(buffer)
     }
 
+
+
     pub fn size(&mut self) -> usize {
         match self.cached_size {
             None => {
-                self.cached_size = Some(self.serialize().unwrap().len() as usize);
+                self.cached_size = Some(self._serialize().unwrap().len() as usize);
                 self.cached_size.unwrap()
             }
             Some(cached_size) => cached_size,
@@ -67,10 +81,11 @@ where
     }
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct SlottedPage<K, V> {
     header: PageHeader<K, V>,
     offsets: Vec<usize>,
-    cells: Vec<Cell<K, V>>,
+    cells: HashMap<u16, Cell<K, V>>,
     free_list: Vec<usize>,
 }
 
@@ -83,34 +98,77 @@ where
         Ok(Self {
             header: PageHeader::new(page_id, right),
             offsets: Vec::new(),
-            cells: Vec::new(),
+            cells: HashMap::new(),
             free_list: Vec::new(),
         })
     }
 
-    pub fn can_insert(&self, cell: &mut Cell<K, V>) -> bool {
+    pub fn can_insert(&self, cell: &mut Cell<K, V>) -> Result<Position> {
         let required_size = cell.size();
         if PAGE_SIZE - self.header.page_size >= required_size {
-            true
+            Ok(Position::Free(required_size as u16))
         } else {
-            self.free_list.iter().any(|&size| size >= required_size)
+            let r = self
+                .free_list
+                .iter()
+                .find(move |&&size| size >= required_size);
+            match r {
+                Some(&size) => Ok(Position::Occupied(size as u16)),
+                None => Err(anyhow::anyhow!("Not enough space to insert the cell")),
+            }
         }
     }
 
     pub fn insert(&mut self, key: K, value: V) -> Result<()> {
-
-        
         let mut cell = Cell::new(key, value)?;
-        if self.can_insert(&mut cell) == false {
-            anyhow::bail!("Not enough space to insert the cell");
+        match self.can_insert(&mut cell) {
+            Ok(pos) => match pos {
+                Position::Free(size) => {
+                    self.header.offset -= size;
+                    self.header.page_size += size as usize;
+                    self.offsets.push(size as usize);
+                    self.cells.insert(size, cell);
+                }
+                Position::Occupied(size) => {
+                    let index = self
+                        .free_list
+                        .iter()
+                        .position(|&s| s == size as usize)
+                        .unwrap();
+                    self.free_list.remove(index);
+                    self.header.page_size += size as usize;
+                    self.offsets.push(size as usize);
+                    self.cells.insert(size, cell);
+                }
+            },
+            Err(_) => anyhow::bail!("Not enough space to insert the cell"),
         }
 
-        
-        self.cells.push(cell);
-
-        let offset = self.cells.len() - 1;
-        self.offsets.push(offset);
-
+        self.offsets.sort_by(|a, b| {
+            let cell_a = self.cells.get(&(*a as u16)).unwrap();
+            let cell_b = self.cells.get(&(*b as u16)).unwrap();
+            cell_a.key.cmp(&cell_b.key)
+        });
         Ok(())
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cell_serialization() {
+        let cell = Cell::new("key1".to_string(), "value1".to_string()).unwrap();
+        let serialized = cell._serialize().unwrap();
+        assert!(!serialized.is_empty());
+    }
+
+    // #[test]
+    // fn test_cell_deserialization() {
+    //     let cell = Cell::new("key1".to_string(), "value1".to_string()).unwrap();
+    //     let serialized = cell._serialize().unwrap();
+    //     // let deserialized: Cell<String, String> = bincode::serde::deserialize(&serialized).unwrap();
+    //     assert_eq!(cell, deserialized);
+    // }
 }
