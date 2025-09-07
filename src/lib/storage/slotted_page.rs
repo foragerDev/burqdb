eee267d39b2aa41ffd5cb5ce717afa08a9a8a8e5
Mohsan <mohsan0073@gmail.com>
2025-09-08 02:19:29 +0700

    Refactor SlottedPage and PageHeader to include page type; adjust page size handling and improve insert logic

--- src/lib/storage/slotted_page.rs
+++ src/lib/storage/slotted_page.rs
@@ -3,19 +3,26 @@ use std::collections::HashMap;
 use anyhow::Result;
 use bincode::{config::standard, serde::encode_to_vec};
 use serde::{Deserialize, Serialize};
-static PAGE_SIZE: usize = 4096;
+
+static PAGE_SIZE: u16 = 4095;
+
+//Currently let's keep it simple with only one page type later we can implement index pages
+#[derive(Serialize, Deserialize, Clone, Copy, PartialEq, Eq)]
+pub enum PageType {
+    Leaf,
+    Internal,
+}
 
 #[derive(Serialize, Deserialize)]
-
 pub struct PageHeader<K, V> {
+    page_type: PageType,
     page_id: u64,
     right: Option<Box<SlottedPage<K, V>>>,
-    page_size: usize,
+    page_size: u16,
     offset: u16,
 }
 
 #[derive(Serialize, Deserialize)]
-
 pub enum Position {
     Free(u16),
     Occupied(u16),
@@ -26,12 +33,13 @@ where
     K: Serialize + for<'de> Deserialize<'de> + Ord,
     V: Serialize + for<'de> Deserialize<'de>,
 {
-    pub fn new(page_id: u64, right: Option<Box<SlottedPage<K, V>>>) -> Self {
+    pub fn new(page_id: u64, right: Option<Box<SlottedPage<K, V>>>, page_type: PageType) -> Self {
         Self {
+            page_type,
             page_id,
             right,
-            page_size: size_of::<PageHeader<K, V>>(),
-            offset: PAGE_SIZE as u16 - 1,
+            page_size: 0,
+            offset: PAGE_SIZE as u16,
         }
     }
 }
@@ -61,7 +69,7 @@ where
         let value_bytes = encode_to_vec(&self.value, standard()).unwrap();
         let mut buffer = Vec::new();
 
-        buffer.extend_from_slice(&(key_bytes.len() as u32).to_le_bytes());
+        // buffer.extend_from_slice(&(key_bytes.len() as u32).to_le_bytes());
         buffer.extend_from_slice(&key_bytes);
         buffer.extend_from_slice(&value_bytes);
 
@@ -92,24 +100,36 @@ where
     K: Serialize + for<'de> Deserialize<'de> + Ord,
     V: Serialize + for<'de> Deserialize<'de>,
 {
-    pub fn new(page_id: u64, right: Option<Box<SlottedPage<K, V>>>) -> Result<Self> {
+    pub fn new(
+        page_id: u64,
+        right: Option<Box<SlottedPage<K, V>>>,
+        page_type: PageType,
+    ) -> Result<Self> {
         Ok(Self {
-            header: PageHeader::new(page_id, right),
+            header: PageHeader::new(page_id, right, page_type),
             offsets: Vec::new(),
             cells: HashMap::new(),
             free_list: Vec::new(),
         })
     }
 
+    pub fn page_id(&self) -> u64 {
+        self.header.page_id
+    }
+
     pub fn can_insert(&self, cell: &mut Cell<K, V>) -> Result<Position> {
-        let required_size = cell.size();
-        if PAGE_SIZE - self.header.page_size >= required_size {
-            Ok(Position::Free(required_size as u16))
+        // Cell Size + Two bytes need in the offsets
+        let required_bytes = cell.size() as u16;
+        let header_side =
+            size_of::<PageHeader<K, V>>() as u16 + (self.offsets.len() as u16 + 1) * 2;
+
+        if self.header.offset - header_side >= required_bytes + 2 {
+            Ok(Position::Free(required_bytes as u16))
         } else {
             let r = self
                 .free_list
                 .iter()
-                .find(move |&&size| size >= required_size);
+                .find(move |&&size| size >= required_bytes as usize);
             match r {
                 Some(&size) => Ok(Position::Occupied(size as u16)),
                 None => Err(anyhow::anyhow!("Not enough space to insert the cell")),
@@ -117,19 +137,18 @@ where
         }
     }
 
-    // pub fn remove(&mut self, key: &K) -> Result<()> {
-
-    // }
+    pub fn remove(&mut self, key: &K) -> Result<()> {
+        Ok(())
+    }
 
     pub fn insert(&mut self, key: K, value: V) -> Result<()> {
         let mut cell = Cell::new(key, value)?;
         match self.can_insert(&mut cell) {
             Ok(pos) => match pos {
                 Position::Free(size) => {
-                    self.header.offset -= size;
-                    let offset = self.header.offset;
-                    self.header.offset -= size;
-                    self.header.page_size += size as usize;
+                    self.header.offset -= size + 1;
+                    let offset = self.header.offset + 1;
+                    self.header.page_size += size;
                     self.offsets.push(offset as usize);
                     self.cells.insert(offset, cell);
                 }
@@ -140,7 +159,7 @@ where
                         .position(|&s| s == at as usize)
                         .unwrap();
                     self.free_list.remove(index);
-                    self.header.page_size += at as usize;
+                    self.header.page_size += at;
                     self.offsets.push(at as usize);
                     self.cells.insert(at, cell);
                 }
@@ -148,6 +167,7 @@ where
             Err(_) => anyhow::bail!("Not enough space to insert the cell"),
         }
 
+        self.header.page_size += 1;
         self.offsets.sort_by(|a, b| {
             let cell_a = self.cells.get(&(*a as u16)).unwrap();
             let cell_b = self.cells.get(&(*b as u16)).unwrap();
@@ -208,6 +228,7 @@ where
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
@@ -221,24 +242,24 @@ mod tests {
     fn test_cell_size() {
         let mut cell: Cell<String, String> =
             Cell::new("key12".to_string(), "value1".to_string()).unwrap();
-        assert_eq!(cell.size(), 17);
+        assert_eq!(cell.size(), 13);
     }
 
     #[test]
     fn test_cell_size_int() {
         let mut cell: Cell<i32, i32> = Cell::new(1, 1).unwrap();
-        assert_eq!(cell.size(), 8);
+        assert_eq!(cell.size(), 2);
     }
     #[test]
     fn test_add_cell() {
-        let mut page = SlottedPage::new(0, None).unwrap();
+        let mut page = SlottedPage::new(0, None, PageType::Internal).unwrap();
         let result = page.insert("key1".to_string(), "value1".to_string());
         assert!(result.is_ok());
     }
 
     #[test]
     fn test_find_cell() {
-        let mut page = SlottedPage::new(0, None).unwrap();
+        let mut page = SlottedPage::new(0, None, PageType::Internal).unwrap();
         page.insert("key1".to_string(), "value1".to_string())
             .unwrap();
         let cell = page.find_cell(&"key1".to_string());
@@ -247,7 +268,7 @@ mod tests {
 
     #[test]
     fn test_multiple_cells() {
-        let mut page = SlottedPage::new(0, None).unwrap();
+        let mut page = SlottedPage::new(0, None, PageType::Internal).unwrap();
         page.insert("key1".to_string(), "value1".to_string())
             .unwrap();
         page.insert("key2".to_string(), "value2".to_string())
@@ -267,7 +288,7 @@ mod tests {
     #[test]
     fn test_offset_order() {
         println!("test offset order");
-        let mut page = SlottedPage::new(0, None).unwrap();
+        let mut page = SlottedPage::new(0, None, PageType::Internal).unwrap();
         page.insert("b".to_string(), "value1".to_string()).unwrap();
         page.insert("c".to_string(), "value2".to_string()).unwrap();
         page.insert("a".to_string(), "value3".to_string()).unwrap();
@@ -275,6 +296,50 @@ mod tests {
         let s = format!("{:?}", page.offsets.clone());
         println!("{}", s);
         let offsets = page.offsets.clone();
-        assert_eq!(offsets, vec![1, 2, 3]);
+        assert_eq!(offsets, vec![4066, 4086, 4076]);
+    }
+
+    #[test]
+    fn test_full_page() {
+        let mut page = SlottedPage::new(0, None, PageType::Internal).unwrap();
+        let mut index = 0;
+        let mut error = anyhow::anyhow!("No error");
+        while match page.insert(format!("key{}", index), format!("value{}", index)) {
+            Ok(_) => {
+                index += 1;
+                true
+            }
+            Err(err) => {
+                error = err;
+                false
+            }
+        } {}
+        assert!(
+            error
+                .to_string()
+                .contains("Not enough space to insert the cell")
+        );
+    }
+
+    #[test]
+    fn test_insert_in_freelist() {
+        let mut page = SlottedPage::new(0, None, PageType::Internal).unwrap();
+        let mut index = 0;
+        let mut error = anyhow::anyhow!("No error");
+        while match page.insert(format!("key{}", index), format!("value{}", index)) {
+            Ok(_) => {
+                index += 1;
+                true
+            }
+            Err(err) => {
+                error = err;
+                false
+            }
+        } {}
+
+        let delete_cells = vec!["key11", "key10", "key4"];
+        for key in delete_cells {
+            // page.remove(&key);
+        }
     }
 }
